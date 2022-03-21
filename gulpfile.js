5bb2b86591105efc6985292dada1befe1c40f4e7
张文皓月 <zhangwenhaoyue@163.com>
2022-03-21 23:38:12 +0800

    优化 gulp上传代码

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ const gulp = require('gulp')
 const sftp = require('@foxtail/gulp-sftp-up4')
 
 const up = () => {
-    return gulp.src('koa-app/**/*')
+    return gulp.src(['koa-app/**/*', '!koa-app/node_modules/**'], {nodir: true})
         .pipe(sftp({
             host: '42.192.195.156',
             user: 'ubuntu',
             pass: 'Weilai.10000',
-            remotePath:'/home/ubuntu/koa-app'
+            remotePath: '/home/ubuntu/koa-app'
         }))
 }
 
