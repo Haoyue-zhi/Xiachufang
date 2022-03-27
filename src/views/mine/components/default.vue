07f9b5617a742941a0b1a9c90646ff4441b81875
张文皓月 <zhangwenhaoyue@163.com>
2022-03-27 23:02:48 +0800

    修复 mine页chekbox动画效果

--- src/views/mine/components/default.vue
+++ src/views/mine/components/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="default">
     <div class="tips">开始准备好好吃饭</div>
-    <van-checkbox v-model="checked" checked-color="#FA8C7C">
+    <van-checkbox v-model="checked" :class="horizontal ? 'shake-horizontal' : '' " checked-color="#FA8C7C">
       我已阅读并同意下厨房的<a>《用户协议》</a>和<a>《隐私政策》</a>
     </van-checkbox>
     <div class="btn">
@@ -11,9 +11,10 @@
 </template>
 
 <script setup>
-import {createApp,ref} from "vue";
-import { useRouter, useRoute } from 'vue-router'
-import { Toast } from 'vant';
+import {createApp, ref} from "vue";
+import {useRouter, useRoute} from 'vue-router'
+import {Toast} from 'vant';
+
 const app = createApp();
 app.use(Toast);
 
@@ -21,10 +22,12 @@ const router = useRouter()
 const route = useRoute()
 
 const checked = ref(false);
+const horizontal = ref(false)
 
 // 登录
 function login() {
   if (checked.value === false) {
+    horizontal.value = true
     Toast({message: '登录/注册前请先阅读并同意相关协议', duration: 1000});
   } else {
     router.push('/mine/verification')
@@ -34,6 +37,7 @@ function login() {
 
 <style scoped lang="scss">
 @import "@/assets/scss/color";
+
 
 .default {
   height: 100%;
